ae27729f37d6b67ede7126e95c88f0f11413e7a9
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-09-16 13:50:32 +0200

    [WIP] Add libdgx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,46 @@
-public class Main
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Main implements ApplicationListener
 {
+    Texture m_backTexture;
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Game start!");
+    }
+
+    @Override
+    public void create() {
+        m_backTexture = new Texture("resources/background.png");
+    }
+
+    @Override 
+    public void dispose()
+    {
+
+    }
+
+    @Override
+    public void resume()
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void render()
+    {
+
+    }
+
+    @Override
+    public void resize(int w, int h)
+    {
+
     }
 }
